e9e2acfe960540b1110c5463a51112e84a01c20c
Kohrak <kohrakgk0h@gmail.com>
2018-03-18 13:08:11 +0000

    add landing page and campgrounds page

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get("/", function(req, res){
     res.render("landing");
 })
 
+app.get("/campgrounds", function(req, res){
+    var campgrounds = [
+        {name:"Magic Camp" , image: "https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144394f3c770a2e5b0_340.jpg"},
+        {name:"Frozen Camp", image: "https://pixabay.com/get/eb35b70b2df6033ed1584d05fb1d4e97e07ee3d21cac104497f1c27faee8bdbd_340.jpg"}
+    ];
+    res.render("campgrounds", {campgrounds: campgrounds});
+})
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Yelp!");
 })
