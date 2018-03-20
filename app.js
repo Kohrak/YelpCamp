36939b460999604ed9e0a4559d935ea1d9ffd28f
Kohrak <kohrakgk0h@gmail.com>
2018-03-20 09:41:45 +0000

    change expired img links

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.get("/", function(req, res){
 
 app.get("/campgrounds", function(req, res){
     var campgrounds = [
-        {name:"Magic Camp" , image: "https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144394f3c770a2e5b0_340.jpg"},
-        {name:"Frozen Camp", image: "https://pixabay.com/get/eb35b70b2df6033ed1584d05fb1d4e97e07ee3d21cac104497f1c27faee8bdbd_340.jpg"}
+        {name:"Magic Camp" , image: "https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144394f5c271a1e9b7_340.jpg"},
+        {name:"Frozen Camp", image: "https://pixabay.com/get/eb35b70b2df6033ed1584d05fb1d4e97e07ee3d21cac104497f1c47aafebb1ba_340.jpg"}
     ];
     res.render("campgrounds", {campgrounds: campgrounds});
 })
