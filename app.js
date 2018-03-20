af217336469500b917384724ddb4660bb0dff7c9
Kohrak <gabrielgt95@gmail.com>
2018-03-20 22:55:35 +0000

    delete campground array and use mongodb

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
-var express = require("express");
-var app = express();
-var bodyParser = (require("body-parser"))
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose")
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+//Schema
+
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
 
 
 app.get("/", function(req, res){
     res.render("landing");
 })
 
-var campgrounds = [
-    {name:"Magic Camp" , image: "https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144394f5c271a1e9b7_340.jpg"},
-    {name:"Frozen Camp", image: "https://pixabay.com/get/eb35b70b2df6033ed1584d05fb1d4e97e07ee3d21cac104497f1c47aafebb1ba_340.jpg"}
-];
-
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+  Campground.find({}, function(err, allCampgrounds){
+    if(err){
+      console.log(err)
+    } else {
+      res.render("campgrounds", {campgrounds: allCampgrounds});
+    }
+  });
+
 })
 
 app.post("/campgrounds", function(req, res){
   var name = req.body.name;
   var image = req.body.image;
-  campgrounds.push({name: name, image: image});
-  res.redirect("/campgrounds");
+  var newCampground = {name: name, image: image}
+  Campground.create(newCampground, function(err, newcamp){
+    if(err){
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  })
+
 })
 
 app.get("/campgrounds/new", function(req, res){
