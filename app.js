2afded4a8ae9684ffe918ced15528fc3a6d8c268
Kohrak <kohrakgk0h@gmail.com>
2018-03-21 12:06:00 +0000

    add show page for campground

--- app.js
+++ app.js
@@ -11,23 +11,28 @@ app.set("view engine", "ejs");
 
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
+//Routes
 
+//Landing Page
 
 app.get("/", function(req, res){
     res.render("landing");
 })
+
+//INDEX campgrounds
 
 app.get("/campgrounds", function(req, res){
   Campground.find({}, function(err, allCampgrounds){
     if(err){
       console.log(err)
     } else {
-      res.render("campgrounds", {campgrounds: allCampgrounds});
+      res.render("index", {campgrounds: allCampgrounds});
     }
   });
 
@@ -36,7 +41,8 @@ app.get("/campgrounds", function(req, res){
 app.post("/campgrounds", function(req, res){
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = {name: name, image: image}
+  var description = req.body.description
+  var newCampground = {name: name, image: image, description: description}
   Campground.create(newCampground, function(err, newcamp){
     if(err){
       console.log(err);
@@ -47,19 +53,35 @@ app.post("/campgrounds", function(req, res){
 
 })
 
+//NEW create new campground
+
 app.get("/campgrounds/new", function(req, res){
   res.render("new.ejs")
 })
 
+//SHOW individual campground
+
+app.get("/campgrounds/:id", function(req, res){
+  var campid = req.params.id
+  Campground.findById(campid, function(err, foundCamp){
+    if (err){
+      console.log(err);
+    } else {
+      res.render("show", {campground: foundCamp});
+    }
+  })
+;
+})
+
 //Cloud9
 
-// app.listen(process.env.PORT, process.env.IP, function(){
-//     console.log("Yelp!");
-// })
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Yelp!");
+})
 
 
 //Local
 
-app.listen(3000, function(){
-    console.log("Yelp!");
-})
+// app.listen(3000, function(){
+//     console.log("Yelp!");
+// })
