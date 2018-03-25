97f8aad43707ba88c039fc890ec569585f4c6a04
Kohrak <kohrakgk0h@gmail.com>
2018-03-25 20:22:28 +0000

    add functionality to create and post new comments

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
     seedDB = require("./seeds");
     
 seedDB();
@@ -25,7 +26,7 @@ app.get("/campgrounds", function(req, res){
     if(err){
       console.log(err)
     } else {
-      res.render("index", {campgrounds: allCampgrounds});
+      res.render("campgrounds/index.ejs", {campgrounds: allCampgrounds});
     }
   });
 
@@ -49,7 +50,7 @@ app.post("/campgrounds", function(req, res){
 //NEW create new campground
 
 app.get("/campgrounds/new", function(req, res){
-  res.render("new.ejs")
+  res.render("campgrounds/new.ejs")
 })
 
 //SHOW individual campground
@@ -60,10 +61,40 @@ app.get("/campgrounds/:id", function(req, res){
     if (err){
       console.log(err);
     } else {
-      res.render("show", {campground: foundCamp});
+      res.render("campgrounds/show", {campground: foundCamp});
     }
   })
 ;
+})
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+  Campground.findById(req.params.id, function(err, campground){
+    if (err){
+      console.log(err)
+    } else {
+        res.render("comments/new.ejs", {campground: campground})
+    }
+  })
+
+})
+
+app.post("/campgrounds/:id/comments", function(req, res){
+  Campground.findById(req.params.id, function(err, campground){
+    if(err){
+      console.log(err)
+      res.redirect("/campgrounds")
+    } else {
+      Comment.create(req.body.comment, function(err, comment){
+        if (err){
+          console.log(err)
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      })
+    }
+  })
 })
 
 //Cloud9
