89eb2d72c7c6e4b7c259f690fa80fbd9529a1a5a
Kohrak <gabrielgt95@gmail.com>
2018-03-28 18:15:35 +0100

    add logout logic

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ app.post("/register", function(req, res){
   })
 })
 
-//show isLoggedIn
+//show log in
 app.get("/login", function(req, res){
   res.render("login");
 })
@@ -145,6 +145,14 @@ app.post("/login", passport.authenticate("local",
       failureRedirect: "/login"
   }) ,function(req, res){
 });
+
+//logout
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/campgrounds");
+})
+
 //Cloud9
 
 // app.listen(process.env.PORT, process.env.IP, function(){
