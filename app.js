a5395a17354ee909a04671aa204d3b9e9e71b01c
Kohrak <gabrielgt95@gmail.com>
2018-03-28 18:20:14 +0100

    add isLoggedIn middleware

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.get("/campgrounds/:id", function(req, res){
 ;
 })
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
   Campground.findById(req.params.id, function(err, campground){
     if (err){
       console.log(err)
@@ -96,7 +96,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
 
 })
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
   Campground.findById(req.params.id, function(err, campground){
     if(err){
       console.log(err)
@@ -152,6 +152,13 @@ app.get("/logout", function(req, res){
   req.logout();
   res.redirect("/campgrounds");
 })
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
 
 //Cloud9
 
