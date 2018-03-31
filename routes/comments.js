a06ad37ecc109bc7b139158f179977912c96f501
Kohrak <gabrielgt95@gmail.com>
2018-03-31 16:29:36 +0100

    add edit comment functionality

--- routes/comments.js
+++ routes/comments.js
@@ -36,6 +36,28 @@ router.post("/", isLoggedIn, function(req, res){
     }
   })
 })
+//edit
+router.get("/:comment_id/edit", function(req, res){
+  Comment.findById(req.params.comment_id, function(err, foundComment){
+    if(err){
+      res.redirect("back");
+    } else {
+      res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+    }
+  })
+})
+
+//update
+
+router.put("/:comment_id", function(req, res){
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedCommment){
+    if(err){
+      res.redirect("/back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  })
+})
 
 
 function isLoggedIn(req, res, next){
