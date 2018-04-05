403d22365f1aa208a4fbb86b8221bcb839efe2dc
Kohrak <kohrakgk0h@gmail.com>
2018-04-05 16:55:19 +0000

    install connect flash

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require("express"),
     passport = require("passport"),
     LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
+    flash = require("connect-flash"),
     User = require("./models/user"),
     Campground = require("./models/campground"),
     Comment = require("./models/comment"),
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
 //seedDB();
 
 
